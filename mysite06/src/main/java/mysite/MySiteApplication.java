8f1e49b3d74af011734a0c8b53bc0dda3ba4641c
Konahyeon <koo@inha.edu>
2025-01-22 09:59:18 +0900

    mysite #2

--- mysite06/src/main/java/mysite/MySiteApplication.java
+++ mysite06/src/main/java/mysite/MySiteApplication.java
@@ -2,10 +2,6 @@ package mysite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-
-import mysite.config.AppConfig;
-import mysite.config.WebConfig;
 
 @SpringBootApplication
 //@Import({AppConfig.class, WebConfig.class})
